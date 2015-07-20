9903644e6a27e48f7db954fb48b57496e86524cd
jpv <thevirus4@hotmail.com>
2015-07-20 13:10:36 +0200

    Parallax toolbar example

--- app/src/main/java/es/jpv/android/examples/collapsibleheaderexample/RecyclerViewFragment.java
+++ app/src/main/java/es/jpv/android/examples/collapsibleheaderexample/RecyclerViewFragment.java
@@ -1,7 +1,6 @@
 package es.jpv.android.examples.collapsibleheaderexample;
 
 import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
